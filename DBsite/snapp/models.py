115e5304b77f4ea17e27ad3d7b5d37273e1303c8
zahrabashir98 <zahra_9877@yahoo.com>
2019-06-06 02:55:58 +0430

    Add Trip,Car,Place and other models

--- DBsite/snapp/models.py
+++ DBsite/snapp/models.py
@@ -14,15 +14,11 @@ class Person(models.Model):
     ('Female', 'Female'),
 ]
 
-    gender = models.CharField(
-        max_length=6,
-        choices=GENDER,
-        
-    )
+    gender = models.CharField(max_length=6, choices=GENDER)
     id_no = models.AutoField(primary_key=True)
-    address = models.CharField(max_length=200, null = True)
+    address = models.CharField(max_length=200)
     # add regex
-    phone_number = models.CharField(max_length=15,  null = True)
+    phone_number = models.CharField(max_length=15)
 
     def __str__(self):
         return (self.first_name, self.last_name)
@@ -43,6 +39,7 @@ class Driver(Person):
     salary = models.IntegerField()
     driver_id = models.IntegerField()
     education = models.CharField(max_length = 20)
+    employee_of_driver = models.ForeignKey('Employee', on_delete=models.CASCADE)
 
     def __str__(self):
         return (self.first_name, self.last_name)
@@ -55,3 +52,56 @@ class Passenger(Person):
 
     def __str__(self):
         return (self.first_name, self.last_name)
+
+
+class Trip(models.Model):
+
+    trip_id = models.IntegerField()
+    selected_addresses = models.CharField(max_length = 200)
+    source = models.ForeignKey('Place', on_delete=models.CASCADE)
+    car_of_trip = models.ForeignKey('Car', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.trip_id
+
+
+class Place(models.Model):
+
+    place_id = models.IntegerField()
+    trafficful_hours = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.place_id
+
+
+class Car(models.Model):
+
+    plaque = models.AutoField(primary_key=True)
+    driver_of_car = models.ForeignKey('Driver', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.place_id
+
+
+class Comment(models.Model):
+
+    text = models.CharField(max_length=300)
+    passenger_which_comments = models.ForeignKey('Passenger', on_delete=models.CASCADE)
+    driver_who_was_commented = models.ForeignKey('Driver', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+class DPP(models.Model):
+
+    passenger_id = models.ForeignKey('Passenger', on_delete=models.CASCADE)
+    driver_id = models.ForeignKey('Driver', on_delete=models.CASCADE)
+    trip_id = models.ForeignKey('Trip', on_delete=models.CASCADE)
+
+
+class Destination(models.Model):
+    related_trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
+    related_place = models.ForeignKey('Place', on_delete=models.CASCADE)
+
+

c4738294d3c6dae501523e1394bb2d78150b643c
zahrabashir98 <zahra_9877@yahoo.com>
2019-06-06 01:53:17 +0430

    Add Employee, Driver, Passenger models

--- DBsite/snapp/models.py
+++ DBsite/snapp/models.py
@@ -3,11 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
 
     pub_date = models.DateTimeField('date published')
     GENDER = [
@@ -21,6 +20,38 @@ class Person(models.Model):
         
     )
     id_no = models.AutoField(primary_key=True)
-    
+    address = models.CharField(max_length=200, null = True)
+    # add regex
+    phone_number = models.CharField(max_length=15,  null = True)
+
+    def __str__(self):
+        return (self.first_name, self.last_name)
 
 
+class Employee(Person):
+
+    salary = models.IntegerField()
+    employee_id = models.IntegerField()
+    education = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return (self.first_name, self.last_name)
+
+
+class Driver(Person):
+
+    salary = models.IntegerField()
+    driver_id = models.IntegerField()
+    education = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return (self.first_name, self.last_name)
+
+
+class Passenger(Person):
+
+    passenger_id = models.IntegerField()
+    selected_addresses = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return (self.first_name, self.last_name)
